41c1d46d47820875d404e12cf503e1e2b30e8190
alphaoumardev <alphaoumardev@outlook.com>
2022-11-26 11:16:47 +0800

    None

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ module.exports = withMT({
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
     'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
-    './src/**/*.{html,js}', './node_modules/tw-elements/dist/js/**/*.js'
+    './src/**/*.{html,js}',
+    './node_modules/tw-elements/dist/js/**/*.js'
   ],
   theme: {
     extend: {},
